254c6649184e260d3a95e2859fa93fd55a6d0b6f
BigMel <139045418+BigMel1388@users.noreply.github.com>
2024-11-19 14:24:23 +0700

    yes

--- DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab580f0c80d49f0d52d77aed777856fa68df372")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
